59bf29e764ea74379fb3fa56b4158b543791015f
jihwooon <jihwooon@gmail.com>
2023-04-01 23:05:12 +0900

    1-1문제를 풀어라
    8진수를 Integer.toOctalString()을 사용하지 않고 풀었습니다.

--- ahnjihwan/app/src/main/java/core/ch1/Example01.java
+++ ahnjihwan/app/src/main/java/core/ch1/Example01.java
@@ -17,24 +17,10 @@ public class Example01 {
     // 정수를 읽어서 8진수로 풀어라
     public String octalNumber(int number) {
         String answer = "";
-        if (number >= 0 && number < 8) {
-            answer = Integer.toString(number);
-            return answer;
-        }
 
-        if (number >= 8 && number < 16) {
-            answer = Integer.toString(number + (2 * 1));
-            return answer;
-        }
-
-        if (number >= 16 && number < 24) {
-            answer = Integer.toString(number + (2 * 2));
-            return answer;
-        }
-
-        if (number >= 24 && number < 32) {
-            answer = Integer.toString(number + (2 * 3));
-            return answer;
+        if (number >= 1) {
+            answer = number % 8 + answer;
+            return octalNumber(number / 8) + answer;
         }
 
         return answer; // 어떻게 하면 중복을 드러낼 수 있을까?
--- ahnjihwan/app/src/test/java/core/ch1/Example02Test.java
+++ ahnjihwan/app/src/test/java/core/ch1/Example02Test.java
@@ -1,2 +1,15 @@
-package core.ch1;public class Example02Test {
+package core.ch1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// 정수(양의)
+public class Example02Test {
+
+
+    @Test
+    public void result() {
+        assertThat("1").isEqualTo("1");
+    }
 }
